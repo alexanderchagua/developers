e4958de8252d7ad5547050308ae88b3dd6e3db19
alexander <alexanderelton2887@gmail.com>
2024-08-28 23:20:39 -0500

    agregamos variables de entorno

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const http = require("http")
 function requestController(){
     console.log("hola mundo!!!!!!!")
 }
+
+const PORT = process.env.PORT 
 //configurar nuestro servidor
 const server = http.createServer(requestController)
 
-server.listen(4000)
+server.listen(4000, function(){
+    console.log({env: process.env})
+})
